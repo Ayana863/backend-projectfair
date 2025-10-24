9695ee8f37aac15bb264a196e5f29f1a9bec4a30
Ayana863 <ayana863anna@gmail.com>
2025-10-24 09:52:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,6 +25,10 @@ pfServer.use(router);
 // pfServer.use('./Uploads',express.static('./Uploads'))
 pfServer.use('/Uploads', express.static(path.join(__dirname, 'Uploads')));
 
+pfServer.get("/", (req, res) => {
+  res.send("Backend is running successfully ");
+});
+
 // create and run server on PORT
 const PORT = process.env.PORT || 3000;
 
